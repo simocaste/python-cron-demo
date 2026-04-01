d83cef10129d1ea9f84f5b50ab08bd1b038fb961
simocaste <simo.caste24@gmail.com>
2026-04-01 22:11:54 +0200

    Add files via upload

---
+++ test_dataframe.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# Creazione del DataFrame
+data = {
+    "top_adunit": ["corriere.it"],
+    "impressions": [1000]
+}
+
+df = pd.DataFrame(data)
+
+# Scrittura su CSV
+#df.to_csv("adunits.csv", index=False)
+
+print("CSV creato con successo!")
